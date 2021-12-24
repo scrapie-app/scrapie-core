48a2cee66ace38b9b0218725a4e7f70df855bae9
Aditya Karnam <akarnam37@gmail.com>
2021-12-24 03:57:34 -0600

    feat: add redis

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import uvicorn
 from src import app
 import logging
 from logging.config import fileConfig
+import dotenv, redis, os
+
+dotenv.load_dotenv()
+redisConnectionPool = redis.ConnectionPool(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT'))
 
 # fileConfig('logging_config.ini')
 options = {
-    'appLogger': logging.getLogger()
+    'appLogger': logging.getLogger(),
+    'redis': redis.Redis(connection_pool=redisConnectionPool)
 }
 scrapieApi = app.App(options)
 
