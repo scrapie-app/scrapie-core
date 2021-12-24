bb8b1f028fdc5fbae4c340f53dd760f3444bb689
Aditya Karnam <akarnam37@gmail.com>
2021-12-24 03:58:05 -0600

    fix: password length to 512

--- database/models.py
+++ database/models.py
@@ -6,7 +6,7 @@ class User(Base):
   __tablename__ = 'users'
   id = Column(Integer, primary_key=True, index=True)
   email = Column(String, unique=True, index=True)
-  hashed_password = Column(String)
+  hashed_password = Column(String(512))
   created_at = Column(DateTime, index=True)
   updated_at = Column(DateTime, index=True)
 
