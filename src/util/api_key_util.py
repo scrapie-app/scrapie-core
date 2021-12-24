a94b7afb6dc6840119e9bb6369a770327ffe71e0
Aditya Karnam <akarnam37@gmail.com>
2021-12-24 00:57:45 -0600

    feat: exclude escaping, problematic chars

--- src/util/api_key_util.py
+++ src/util/api_key_util.py
@@ -1,12 +1,15 @@
 from random import random
 import math
 # valid characters range from ascii 33 to 126
+# exclude special characters 34, 39, 58, 59, 91-96
+exclude_characters = [34, 39, 58, 59, 91, 92, 93, 94, 95, 96]
 
 def generate_api_key(keyLength):
   if keyLength < 0:
     raise Exception("Invalid API Key length")
   api_key = ""
-  for i in range(0, keyLength):
-    randomChar = chr(33 + math.floor(93 * random()))
-    api_key += randomChar
+  while len(api_key) != keyLength:
+    randomCharAscii = 33 + math.floor(93 * random())
+    if randomCharAscii not in exclude_characters:
+      api_key += chr(randomCharAscii)
   return api_key
