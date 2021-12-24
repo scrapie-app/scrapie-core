3274950315dd1b1a82eaa8e2f6c177088f2d6c0c
Aditya Karnam <akarnam37@gmail.com>
2021-12-24 03:59:10 -0600

    fix: use bcrypt password hasing

--- src/routes/users.py
+++ src/routes/users.py
@@ -2,11 +2,10 @@ from fastapi import HTTPException, Depends
 from sqlalchemy.orm import Session
 from schema import user as UserSchema
 from database import models
-import hashlib, uuid
-from ..util import api_key_util
+from ..util import api_secrets_util
 from time import time
 from datetime import datetime
-
+import bcrypt
 
 class UserRoute:
     def __init__(self, app, options) -> None:
@@ -32,9 +31,7 @@ class UserRoute:
         db_user = db.query(models.User).filter(models.User.email == user.email).first()
         if db_user is not None:
           raise HTTPException(status_code=400, detail="User already exists")
-        salt = uuid.uuid4().hex
-        encoded_string = (user.password + salt).encode('utf-8')
-        hashed_password = hashlib.sha512(encoded_string).hexdigest()
+        hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
         time_now = datetime.fromtimestamp(time())
         db_user = models.User(email=user.email, hashed_password=hashed_password, created_at=time_now, updated_at=time_now)
         db.add(db_user)
@@ -42,7 +39,7 @@ class UserRoute:
         db.flush()
         # get the user id and create a quota for the user
         # generate the API key here
-        api_key = api_key_util.generate_api_key(24)
+        api_key = api_secrets_util.generate_api_key(24)
         db_user_api_quota = models.APIQuota(user_id=db_user.id, api_key=api_key, quota=100, created_at=time_now, updated_at=time_now)
         db.add(db_user_api_quota)
         db.commit()
